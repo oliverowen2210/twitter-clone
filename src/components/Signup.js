13e6ca40f6f1b0c3082880a47f6b414b0a6d8781
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-22 18:44:25 -0600

    improved styling on signup box

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,15 @@
 export default function Signup(props) {
   return (
-    <div className="p-2 border-[1px] border-solid border-gray-200 rounded-lg">
-      <h2 className="text-lg font-bold">New to Twitter?</h2>
-      <p className=" text-sm font-heavy text-gray-500">
+    <div className="pt-[12px] pb-[64px] border-[1px] border-solid border-gray-200 rounded-[16px] mb-[16px]">
+      <h2 className="text-lg font-bold py-[12px] px-[16px]">New to Twitter?</h2>
+      <p className=" text-sm font-heavy text-gray-500 pl-[12px] pr-[12px] text-[13px]">
         Sign up now to get your own personalized timeline!
       </p>
-      <a href="/signup">
-        <button>Sign up</button>
-      </a>
+      <div className="my-[16px] mx-[12px]">
+        <a href="/signup">
+          <button>Sign up</button>
+        </a>
+      </div>
     </div>
   );
 }
