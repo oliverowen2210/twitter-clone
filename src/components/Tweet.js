e35f30ed625be8ef18a5bb1fbb0f32554d8d0551
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-26 14:36:59 -0600

    made Tweet elements link to use profile/tweet page

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,19 +5,22 @@ import Share from "./Share";
 
 export default function Tweet(props) {
   return (
-    <div className="w-[600px] px-4 py-2 border-b-[1px] border-gray-200 border-solid">
+    <a
+      href="/tweet/123"
+      className="duration-100 hover:bg-gray-100 block w-[600px] px-4 py-2 border-b-[1px] border-gray-200 border-solid"
+    >
       <div className="flex ">
-        <a href="/test">
+        <a href={`/${props.handle}`}>
           <div
             className={`profilepic rounded-full penguin`}
             alt="profile pic"
           />
         </a>
         <div className="w-full">
-          <div className="flex">
+          <a className="block flex w-fit" href={`/${props.handle}`}>
             <h3 className="font-bold">{props.author}</h3>
             <p className="handle ml-1 text-gray-500">{props.handle}</p>
-          </div>
+          </a>
           <div>
             <p className="leading-snug break-words">{props.content}</p>
           </div>
@@ -31,6 +34,6 @@ export default function Tweet(props) {
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
