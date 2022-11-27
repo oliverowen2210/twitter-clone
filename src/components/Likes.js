a51e24e891d21c4ec656f16f019f6445fc9fec79
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-27 17:00:26 -0600

    added unliking

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { doc, getDoc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import { doc, getDoc, deleteField, updateDoc } from "firebase/firestore";
 
 import { UserContext, DBContext, ModalContext } from "./App";
 import TweetButton from "./TweetButton";
@@ -11,21 +11,40 @@ export default function Likes(props) {
   const setModal = useContext(ModalContext);
 
   const [count, setCount] = useState(props.count);
+  let locked = false;
   return (
     <TweetButton
       clickFunc={async () => {
+        if (locked) return;
         if (!user) {
           setModal(true);
           return;
         }
-        let dateLiked = new Date();
-        let tweetDocRef = doc(db, "tweets", props.data.id);
+        const dateLiked = new Date();
 
-        let tweetDocSnap = await getDoc(tweetDocRef);
-        if (!!tweetDocSnap.data().likes) {
-          if (!!tweetDocSnap.data().likes[user.uid]) console.log("unlike");
+        locked = true;
+        const userDocRef = doc(db, "users", user.uid);
+        const tweetDocRef = doc(db, "tweets", props.data.id);
+
+        const tweetDocSnap = await getDoc(tweetDocRef);
+        /** check if tweet has likes and if user already liked it */
+
+        if (!!Object.keys(tweetDocSnap.data().likes).length) {
+          if (!!tweetDocSnap.data().likes[user.uid]) {
+            await updateDoc(userDocRef, {
+              [`likes.${props.data.id}`]: deleteField(),
+            });
+
+            await updateDoc(tweetDocRef, {
+              [`likes.${user.uid}`]: deleteField(),
+            });
+
+            /**update counter without refreshing page */
+            await setCount(count - 1);
+
+            locked = false;
+          }
         } else {
-          const userDocRef = doc(db, "users", user.uid);
           await updateDoc(userDocRef, {
             likes: {
               [props.data.id]: { likedOn: dateLiked, id: props.data.id },
@@ -41,7 +60,8 @@ export default function Likes(props) {
               },
             },
           });
-          setCount(props.count + 1);
+          await setCount(count + 1);
+          locked = false;
         }
       }}
       color="pink-500"
