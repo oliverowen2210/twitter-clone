89cd9ca937e1d899eef921db7d781fdc9757e083
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-29 22:33:15 -0600

    check if tweet is retweet and highlight if so

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,28 +1,65 @@
+import { useState, useEffect, useContext } from "react";
+import { getDoc, doc } from "firebase/firestore";
+
+import { UserContext, DBContext } from "./App";
 import Tweet from "./Tweet";
 import uniqid from "uniqid";
 
 export default function Tweets(props) {
+  let [tweetList, setTweetList] = useState([]);
+
+  const user = useContext(UserContext);
+  const db = useContext(DBContext);
+
+  useEffect(() => {
+    async function getTweet(id) {
+      const docRef = doc(db, "tweets", id);
+      const tweetDoc = await getDoc(docRef);
+      return tweetDoc.data();
+    }
+    async function updateRetweets() {
+      let copyTweetList = [...props.tweets];
+      for (let tweet of copyTweetList) {
+        if (!!tweet.originalID) {
+          const originalTweet = await getTweet(tweet.originalID);
+          tweet.likes = originalTweet.likes;
+          tweet.retweets = originalTweet.retweets;
+          tweet.replies = originalTweet.replies;
+        }
+      }
+      setTweetList(copyTweetList);
+    }
+
+    updateRetweets();
+  }, [props.tweets, db]);
+
   return (
     <ul>
-      {props.tweets.length
-        ? props.tweets.map((tweet) => (
-            <Tweet
-              data={{
-                author: tweet.author,
-                content: tweet.content,
-                datePosted: tweet.datePosted,
-                handle: tweet.handle,
-                id: tweet.id,
-                likes: tweet.likes,
-                originalID: tweet.originalID,
-                retweetID: tweet.retweetID,
-                replies: tweet.replies,
-                replyTo: tweet.replyTo,
-                retweets: tweet.retweets,
-              }}
-              key={uniqid()}
-            />
-          ))
+      {tweetList.length
+        ? tweetList.map((tweet) => {
+            let tweetData = {
+              author: tweet.author,
+              content: tweet.content,
+              datePosted: tweet.datePosted,
+              handle: tweet.handle,
+              id: tweet.id,
+              likes: tweet.likes,
+              originalID: tweet.originalID,
+              replies: tweet.replies,
+              replyTo: tweet.replyTo,
+              retweets: tweet.retweets,
+            };
+            let retweeted = false;
+            if (tweet.retweetedBy) {
+              tweetData.retweetedBy = tweet.retweetedBy;
+              if (tweetData.retweetedBy === user.username) {
+                retweeted = true;
+              }
+            }
+            return (
+              <Tweet data={tweetData} key={uniqid()} retweeted={retweeted} />
+            );
+          })
         : null}
     </ul>
   );
