36ce154b7b2116f713fb7eb233bc395e6bcbb44c
oliverowen2210 <oliverowen2210@gmail.com>
2022-12-05 20:59:27 -0600

    moved retweet function out of onClick

--- src/components/Retweets.js
+++ src/components/Retweets.js
@@ -25,102 +25,107 @@ export default function Retweets(props) {
     props.highlight || (user && !!user.retweets[props.data.id])
   );
   let locked = useRef(null);
+
+  async function retweet() {
+    if (locked.current) return;
+    if (!user) {
+      setModal(true);
+      return;
+    }
+    const dateRetweeted = new Date();
+
+    locked.current = true;
+    const userDocRef = doc(db, "users", user.uid);
+    let originalTweetID = props.data.originalID
+      ? props.data.originalID
+      : props.data.id;
+    const originalTweetDocRef = doc(db, "tweets", originalTweetID);
+    const originalTweetDocSnap = await getDoc(originalTweetDocRef);
+
+    let retweetID;
+
+    if (!originalTweetDocSnap.data().retweets[user.uid]) {
+      /**retweet */
+
+      const retweetDoc = doc(collection(db, "tweets"));
+      retweetID = retweetDoc.id;
+
+      let retweetData = { ...props.data };
+      retweetData.originalID = retweetData.originalID
+        ? retweetData.originalID
+        : retweetData.id;
+      retweetData.id = retweetID;
+      retweetData.retweetedBy = user.username;
+
+      await setDoc(doc(db, "tweets", retweetID), retweetData);
+
+      await updateDoc(userDocRef, {
+        [`tweets.${retweetID}`]: {
+          id: retweetData.id,
+          originalID: originalTweetID,
+          retweetedBy: user.username,
+        },
+        [`retweets.${originalTweetID}`]: {
+          retweetedOn: dateRetweeted,
+          retweetID: retweetData.id,
+          originalID: originalTweetID,
+        },
+      });
+
+      await updateDoc(originalTweetDocRef, {
+        [`retweets.${user.uid}`]: {
+          retweetedOn: dateRetweeted,
+          username: user.username,
+          handle: user.handle,
+          uid: user.uid,
+          retweetID: retweetData.id,
+        },
+      });
+
+      await setCount(count + 1);
+      await setHighlight(true);
+
+      /**reloads page to avoid having to link twinks to their retweets
+       * and vice versa. maybe someday?
+       */
+      if (props.originalVisible || !Object.keys(props.data.retweets).length) {
+        window.location.reload();
+      } else {
+        locked.current = false;
+      }
+    } else {
+      /** unretweet*/
+      let retweetID = props.data.retweets[user.uid].retweetID;
+
+      await updateDoc(userDocRef, {
+        [`tweets.${retweetID}`]: deleteField(),
+        [`retweets.${originalTweetID}`]: deleteField(),
+      });
+
+      await updateDoc(originalTweetDocRef, {
+        [`retweets.${user.uid}`]: deleteField(),
+      });
+
+      await deleteDoc(doc(db, "tweets", retweetID));
+
+      await setCount(count - 1);
+      await setHighlight(false);
+
+      if (props.originalVisible) {
+        window.location.reload();
+      } else {
+        locked.current = false;
+      }
+    }
+  }
+
   useEffect(() => {
     locked.current = false;
   });
   return (
     <TweetButton
-      clickFunc={async () => {
-        if (locked.current) return;
-        if (!user) {
-          setModal(true);
-          return;
-        }
-        const dateRetweeted = new Date();
-
-        locked.current = true;
-        const userDocRef = doc(db, "users", user.uid);
-        let originalTweetID = props.data.originalID
-          ? props.data.originalID
-          : props.data.id;
-        const originalTweetDocRef = doc(db, "tweets", originalTweetID);
-        const originalTweetDocSnap = await getDoc(originalTweetDocRef);
-
-        let retweetID;
-
-        if (!originalTweetDocSnap.data().retweets[user.uid]) {
-          /**retweet */
-
-          const retweetDoc = doc(collection(db, "tweets"));
-          retweetID = retweetDoc.id;
-
-          let retweetData = { ...props.data };
-          retweetData.originalID = retweetData.originalID
-            ? retweetData.originalID
-            : retweetData.id;
-          retweetData.id = retweetID;
-          retweetData.retweetedBy = user.username;
-
-          await setDoc(doc(db, "tweets", retweetID), retweetData);
-
-          await updateDoc(userDocRef, {
-            [`tweets.${retweetID}`]: {
-              id: retweetData.id,
-              originalID: originalTweetID,
-              retweetedBy: user.username,
-            },
-            [`retweets.${originalTweetID}`]: {
-              retweetedOn: dateRetweeted,
-              retweetID: retweetData.id,
-              originalID: originalTweetID,
-            },
-          });
-
-          await updateDoc(originalTweetDocRef, {
-            [`retweets.${user.uid}`]: {
-              retweetedOn: dateRetweeted,
-              username: user.username,
-              handle: user.handle,
-              uid: user.uid,
-              retweetID: retweetData.id,
-            },
-          });
-
-          await setCount(count + 1);
-          await setHighlight(true);
-
-          /**reloads page to avoid having to link twinks to their retweets
-           * and vice versa. maybe someday?
-           */
-          if (props.originalVisible) {
-            window.location.reload();
-          } else {
-            locked.current = false;
-          }
-        } else {
-          /** unretweet*/
-          let retweetID = props.data.retweets[user.uid].retweetID;
-
-          await updateDoc(userDocRef, {
-            [`tweets.${retweetID}`]: deleteField(),
-            [`retweets.${originalTweetID}`]: deleteField(),
-          });
-
-          await updateDoc(originalTweetDocRef, {
-            [`retweets.${user.uid}`]: deleteField(),
-          });
-
-          await deleteDoc(doc(db, "tweets", retweetID));
-
-          await setCount(count - 1);
-          await setHighlight(false);
-
-          if (props.originalVisible) {
-            window.location.reload();
-          } else {
-            locked.current = false;
-          }
-        }
+      clickFunc={() => {
+        retweet();
       }}
       color="green-400"
       alt={highlight}
