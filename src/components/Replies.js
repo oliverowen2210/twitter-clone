d4336065c5688aa38f5c68b8965aff5104e9e467
oliverowen2210 <oliverowen2210@gmail.com>
2022-12-09 23:54:30 -0600

    clicking reply button takes you to tweet page

--- src/components/Replies.js
+++ src/components/Replies.js
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import TweetButton from "./TweetButton";
 import SVGs from "../images/SVGs";
 
 export default function Replies(props) {
+  const navigate = useNavigate();
   return (
     <TweetButton
+      clickFunc={() => {
+        navigate(`/tweet/${props.data.id}`);
+      }}
       color="blue-400"
       path={SVGs.chatBubble}
       count={props.count}
