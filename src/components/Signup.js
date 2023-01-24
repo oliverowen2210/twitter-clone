29cf12913338b869ca028db73458dec159e08783
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-23 20:09:04 -0600

    signup button now opens the signup modal

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { ModalsContext } from "./App";
 
 export default function Signup(props) {
+  const openSignupModal = useContext(ModalsContext).signup.toggle;
   return (
     <div className="w-[350px] pt-[12px] pb-[64px] border-[1px] border-solid border-gray-200 rounded-[16px] mb-[16px]">
       <h2 className="text-lg font-bold py-[12px] px-[16px]">New to Twitter?</h2>
@@ -9,11 +10,12 @@ export default function Signup(props) {
         Sign up now to get your own personalized timeline!
       </p>
       <div className="my-[16px] mx-[12px]">
-        <Link to="/signup">
-          <button className="w-[300px] font-bold hover:bg-gray-200 h-[40px] duration-[0.2s] min-w-[36px] min-h-[36px] px-[16px] border-[1px] border-gray-400 rounded-full">
-            Sign up
-          </button>
-        </Link>
+        <button
+          onClick={openSignupModal}
+          className="w-[300px] font-bold hover:bg-gray-200 h-[40px] duration-[0.2s] min-w-[36px] min-h-[36px] px-[16px] border-[1px] border-gray-400 rounded-full"
+        >
+          Sign up
+        </button>
       </div>
     </div>
   );
