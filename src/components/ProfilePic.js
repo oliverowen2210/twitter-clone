fad355c8d01612c449c180c313f7f12ef3c4d893
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-23 19:15:03 -0600

    removed redundant non-Link variant

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -7,6 +7,7 @@ import { StorageContext } from "./App";
 export default function ProfilePic(props) {
   const storage = useContext(StorageContext);
   const [PFPURL, setPFPURL] = useState(null);
+  const [busy, setBusy] = useState(true);
 
   useEffect(() => {
     async function getURL() {
@@ -19,12 +20,13 @@ export default function ProfilePic(props) {
         URL = await getDownloadURL(ref(storage, "defaultPFP.png"));
         setPFPURL(URL);
       }
+      setBusy(false);
     }
     getURL();
   }, [PFPURL, props.storage, props.id, storage]);
 
-  return props.link ? (
-    <Link to={`/${props.link}`} className={"relative z-10"}>
+  return busy ? null : (
+    <Link to={props.link ? `${props.link}` : null} className={"relative z-10"}>
       <div
         style={
           props.src
@@ -42,22 +44,5 @@ export default function ProfilePic(props) {
         alt="profile pic"
       />
     </Link>
-  ) : (
-    <div
-      style={
-        props.src
-          ? { backgroundImage: `url(${props.src})` }
-          : PFPURL
-          ? { backgroundImage: `url("${PFPURL}")` }
-          : null
-      }
-      className={
-        (props.big
-          ? "w-[133.5px] h-[133.5px] "
-          : "mr-[12px] w-[48px] h-[48px] ") +
-        "bg-center bg-cover bg-no-repeat rounded-full"
-      }
-      alt="profile pic"
-    />
   );
 }
