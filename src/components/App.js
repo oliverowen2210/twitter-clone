87aea22d59cd999ca24f7e3edf349c75511f61eb
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-25 23:22:30 -0600

    changed homepage to github-pages link and swapped BrowserRouter for HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext } from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, HashRouter as Router } from "react-router-dom";
 import {
   db,
   auth,
